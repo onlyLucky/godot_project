283509abf30c3d4c10615c32a5f3932815eed1bb
Feynman <2901954062@qq.com>
2024-12-26 15:05:08 +0800

    feat: 修复所有已知问题，新增正负号，clear 去除单个输入逻辑，调整部分功能样式。

--- Calculator/application.gd
+++ Calculator/application.gd
@@ -4,10 +4,31 @@ var cal = Calculator.new()
 @onready var label_current_number = $PanelContainer/MarginContainer/VBoxContainer/LabelCurrentNumber
 @onready var label_result = $PanelContainer/MarginContainer/VBoxContainer/LabelResult
 
+@onready var button_percent = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/GridContainer/ButtonPercent
+@onready var button_symbol = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/GridContainer/ButtonSymbol
+@onready var button_point = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/GridContainer/ButtonPoint
+@onready var button_div = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/ButtonDiv
+@onready var button_mul = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/ButtonMul
+@onready var button_sub = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/ButtonSub
+@onready var button_equal = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/ButtonEqual
+@onready var button_add = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/ButtonAdd
+
+
 func _ready():
 	print("test")
 	cal.current_number_changed.connect(_on_current_number_changed)
 	cal.result_changed.connect(_on_result_changed)
+	cal.is_error_num.connect(_on_is_error_num)
+
+func _on_is_error_num():
+	button_percent.disabled = cal.is_error
+	button_symbol.disabled = cal.is_error
+	button_point.disabled = cal.is_error
+	button_div.disabled = cal.is_error
+	button_mul.disabled = cal.is_error
+	button_sub.disabled = cal.is_error
+	button_equal.disabled = cal.is_error
+	button_add.disabled = cal.is_error
 
 func _on_result_changed():
 	label_result.text = cal.result
@@ -55,7 +76,7 @@ func _on_button_0_pressed():
 
 
 func _on_button_div_pressed():
-	pass # Replace with function body.
+	cal.div()
 
 
 func _on_button_mul_pressed():
@@ -72,3 +93,15 @@ func _on_button_sub_pressed():
 
 func _on_button_equal_pressed():
 	cal.equal()
+
+
+func _on_button_clear_pressed():
+	cal.clear()
+
+
+func _on_button_clear_single_pressed():
+	cal.clearSingle()
+
+
+func _on_button_symbol_pressed():
+	cal.changeSymbol()
--- Calculator/calculator.gd
+++ Calculator/calculator.gd
@@ -3,40 +3,99 @@ extends RefCounted
 
 signal current_number_changed
 signal result_changed
+signal is_error_num # NaN
 
-var result: String = ""
+# 算数是否报错
+var is_error: bool = false:
+	set(v):
+		is_error = v
+		is_error_num.emit()
 
+#最顶上显示label text
+var result: String = "":
+	set(v):
+		result = v
+		result_changed.emit()
+
+# 是否为第一次输入
 var new_number: bool = true
-var current_num: String = "0"
+# 第二行显示label text
+var current_num: String = "0": 
+	set(v):
+		current_num = v
+		current_number_changed.emit()
+	
 var num1: String = ""
 var sign: String = ""
 var num2: String = ""
+# 当前是否为等于
+var isEqual: bool = false
 
 func _init():
+	current_num = "0"
 	print("init")
 
 func input_number(value: int):
+	if is_error or isEqual:
+		clear()
 	if new_number:
-		current_num=""
-		new_number=false
-	current_num+=str(value)
-	current_number_changed.emit()
+		current_num = ""
+		new_number = false
+	if current_num.length()<=0 or current_num=='0':
+		current_num = ""
+	current_num += str(value)
+	if sign:
+		num2 = current_num
+
+func clear():
+	current_num = "0"
+	num1 = ""
+	sign = ""
+	num2 = ""
+	isEqual = false
+	is_error = false
+	get_result()
+	new_number = true
+
+func clearSingle():
+	if isEqual:
+		result = ""
+		return
+	if current_num == '0':
+		return
+	current_num = "0" if current_num.length()<=1 else current_num.substr(0, current_num.length() - 1)
+	
+	if sign:
+		num2 = current_num
+	
+
+func changeSymbol():
+	if current_num == '0':
+		return
+	if current_num.contains("-"):
+		current_num = current_num.replace("-",'')
+	else:
+		current_num = "-" + current_num
+	if sign:
+		num2 = current_num
+	else:
+		num1 = current_num
 
 func calculate_function(_sign: String):
 	new_number = true
-	if not num1:
+	isEqual = false
+	if not num1 :
 		num1 = current_num
-		sign = _sign
-		get_result()
-		result_changed.emit()
-		return
-	if num2:
+	
+	if sign and num2:
+		var res_num = calculate()
+		current_num = res_num
+		num1 = res_num
 		num2 = ""
-	var res_num = calculate()
-	num1 = res_num
-	current_num = res_num
+		new_number = true
+		
+	sign = _sign
 	get_result()
-	result_changed.emit()
 
 func add():
 	calculate_function("+")
@@ -46,24 +105,29 @@ func sub():
 
 func mul():
 	calculate_function("*")
+	
+func div():
+	calculate_function("/")
 
 func equal():
-	if not current_num:
+	if not current_num or not sign:
 		return
 	if not num1:
 		num1 = current_num
 		return
 	if not num2:
 		num2 = current_num
+	isEqual = true
 	var res_num = calculate()
 	current_num = res_num
 	new_number = true
 	get_result()
+	num1 = current_num
 
 func get_result():
-	result=num1+sign
-	if num2:
-		result+=num2+"="
+	result = num1 + " " + sign + " "
+	if isEqual:
+		result += num2 + " = "
 	
 
 func calculate():
@@ -78,4 +142,11 @@ func calculate():
 		res_num = oneValue-twoValue
 	if sign == "*":
 		res_num = oneValue*twoValue
+	if sign == "/":
+		if twoValue == 0:
+			res_num = '除数不能为0'
+			is_error = true
+		else:
+			res_num = oneValue/twoValue
+		
 	return str(res_num)
