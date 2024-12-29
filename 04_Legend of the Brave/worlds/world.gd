404a738ad32171baf4ba7cfacdde0cee9e59dba7
Feynman <290154062@qq.com>
2024-12-29 16:31:20 +0800

    fix: 修复加载游戏无法保存野猪数据

--- 04_Legend of the Brave/worlds/world.gd
+++ 04_Legend of the Brave/worlds/world.gd
@@ -49,5 +49,5 @@ func to_dict() -> Dictionary:
 func from_dict(dict: Dictionary) -> void:
 	for node in get_tree().get_nodes_in_group('enemies'):
 		var path := get_path_to(node)
-		if path not in dict.enemies_alive:
+		if String(path) not in dict.enemies_alive:
 			node.queue_free()
