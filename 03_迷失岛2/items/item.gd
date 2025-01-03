275f29442e3a92642a40d19bfb2942b968e62bf3
Feynman <2901954062@qq.com>
2025-01-03 10:25:18 +0800

    workflow: 调整文件项目目录

--- 03_迷失岛2/items/item.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name Item
-
-# 资源道具类
-@export var description: String
-@export var prop_texture: Texture
-@export var scene_texture: Texture
-
--- 04_Legend of the Brave/classes/Hurtbox.gd
+++
@@ -1,7 +0,0 @@
-class_name Hurtbox
-extends Area2D
-# 受击判定框
-
-# 发送信号 攻击的单位
-signal hurt(hitbox)
-
