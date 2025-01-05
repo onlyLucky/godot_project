23bbeb6122792703cecfdb51b69532153fc7d1f4
Feynman <290154062@qq.com>
2025-01-05 21:26:56 +0800

    feat: 新增文件打开查看图片

--- ResourceExplorer/scenes/viewer/base_panel.gd
+++ ResourceExplorer/scenes/viewer/base_panel.gd
@@ -1,7 +1,14 @@
 class_name BasePanel
 extends PanelContainer
 
+# 基础面板点击信号，查看详情
+signal pressed
+
 # 单个文件 基础面板 后续根据不同类型文件展示不同文件的基础面板
+
+func _ready() -> void:
+	# 连接文件点击按钮事件
+	$FileItemBtn.pressed.connect(func(): pressed.emit())
 
 @onready var tr_main_image: TextureRect = %TR_MainImage as TextureRect
 @onready var default_texture: MarginContainer = %DefaultTexture
--- ResourceExplorer/scenes/viewer/viewer.gd
+++ ResourceExplorer/scenes/viewer/viewer.gd
@@ -38,11 +38,21 @@ func new_panel(image_path: String):
 	#var texture = ImageTexture.create_from_image(image)
 	#panel.set_image(texture)
 	panel.set_title(image_path.get_file())
+	panel.set_meta("image_path",image_path)
+	# 右侧面板点击事件绑定
+	panel.pressed.connect(_on_panel_pressed.bind(panel))
+	
 	thread_helper.join_function(func(): 
 		add_image_from_thread(panel,image_path)
 	)
 	
 	return panel
+
+# Signals 面板点击绑定事件 panel 为右侧单个文件item
+func _on_panel_pressed(panel: BasePanel):
+	var image_path = panel.get_meta("image_path")
+	# 获取每一个点击项，打开文件
+	OS.shell_open(image_path)
 
 # 从线程中加载image
 func add_image_from_thread(panel, image_path):
