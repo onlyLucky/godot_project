c5d118cd114a957dc797ef072fd866a720aa90da
Feynman <290154062@qq.com>
2025-01-05 21:39:11 +0800

    feat: 优化图片文件预览，添加缩略图

--- ResourceExplorer/scenes/viewer/viewer.gd
+++ ResourceExplorer/scenes/viewer/viewer.gd
@@ -7,6 +7,8 @@ extends PanelContainer
 
 # 多线程对象
 var thread_helper:ThreadHelper
+# 图片最大宽度
+var image_max_size := 200
 
 func _ready() -> void:
 	clear()
@@ -48,16 +50,36 @@ func new_panel(image_path: String):
 	
 	return panel
 
-# Signals 面板点击绑定事件 panel 为右侧单个文件item
+# 从线程中加载image
+func add_image_from_thread(panel, image_path):
+	var image = Image.load_from_file(image_path)
+	# 优化图片加载 更改为缩略图
+	image = scale_down(image, image_max_size)
+	var texture = ImageTexture.create_from_image(image)
+	# 有些节点的函数在thread没办法调用 在空闲帧去调用函数
+	panel.call_deferred("set_image",texture)
+
+# 图片缩略图处理函数
+func scale_down(image: Image, max_size: int):
+	var image_size = image.get_size()
+	# 获取图片长宽比
+	var image_aspect = image_size.aspect()
+	if max(image_size.x,image_size.y) <= max_size:
+		return image
+	var new_w = 0
+	var new_h = 0
+	if image_aspect > 1: #w>h
+		new_w = max_size
+		new_h = max_size/image_aspect
+	else:
+		new_w = max_size*image_aspect
+		new_h = max_size
+	image.resize(new_w,new_h)
+	return image
+
+## Signals
+# 面板点击绑定事件 panel 为右侧单个文件item
 func _on_panel_pressed(panel: BasePanel):
 	var image_path = panel.get_meta("image_path")
 	# 获取每一个点击项，打开文件
 	OS.shell_open(image_path)
-
-# 从线程中加载image
-func add_image_from_thread(panel, image_path):
-	var image = Image.load_from_file(image_path)
-	var texture = ImageTexture.create_from_image(image)
-	# 有些节点的函数在thread没办法调用 在空闲帧去调用函数
-	panel.call_deferred("set_image",texture)
-	
